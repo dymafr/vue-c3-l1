2172fdd1d2d723f2c814a82a114f5e2f6fb21f82
dymafr <erwan1309@hotmail.com>
2022-02-16 19:21:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-gcykdv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-gcykdv)
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Bonjour {{ name }}</h1>
+</template>
+
+<script setup lang="ts">
+const name = 'Jean';
+</script>
+
+<style></style>
